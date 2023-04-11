a014f03d8bfe7514e1647d8229fb002f3e65383b
DaichiSawamura <dimakr-303@yandex.ru>
2023-04-11 18:11:17 +0300

    add

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+CREATE TABLE employees
+(
+    first_name text,
+	last_name text,
+	title text,
+    birth_date text,
+    notes text
+);
+SELECT * FROM employees;
+
+CREATE TABLE customers
+(
+    customer_id text,
+	company_name text,
+	contact_name text
+);
+SELECT * FROM customers;
+
+CREATE TABLE orders
+(
+    order_id text,
+	customer_id text,
+	employee_id text,
+	order_date text,
+	ship_city text
+);
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
+import psycopg2
+import csv
+
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+with open("employees_data.csv", "r") as f:
+    next(f)
+    list_empl_date = []
+    for i in f:
+        list_empl_date.append(i.strip().split(","))
+
+with psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='210199'
+) as conn:
+    with conn.cursor() as cur:
+        with open("employees_data.csv") as f:
+            next(f)
+            data = csv.reader(f)
+            for i in data:
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)",
+                            (i[0], i[1], i[2], i[3], i[4]))
+        with open("orders_data.csv") as f:
+            next(f)
+            data = csv.reader(f)
+            for i in data:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                            (i[0], i[1], i[2], i[3], i[4]))
+        with open("customers_data.csv") as f:
+            next(f)
+            data = csv.reader(f)
+            for i in data:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                            (i[0], i[1], i[2]))
+
+conn.close()
